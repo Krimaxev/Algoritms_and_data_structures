f9209f8ba552795585003ba278d6a8240b2f1e6a
Krimaxev <m.krilichevsky@yandex.ru>
2024-09-19 14:30:24 +0300

    Add file

--- n4/n4_test_n3.py
+++ n4/n4_test_n3.py
@@ -1,25 +1,22 @@
 import time
 t_start = time.time()
 
+def LF(x):
+    if x==0: return 0
+    fb1,fb2 = 0,1
+    for i in range(x-1):
+        fb = (fb1+fb2)%10
+        fb1,fb2 = fb2,fb
+    return fb2
+
 with open("n4_input_n3", "r") as f:
     for s in f:
         a = [int(x) for x in s.split()]
-
-c = 0
-x = a[0]
-fb1 = fb2 = 1
-while c < x - 2:
-    if x < 0 or x > 10 ** 7:
-        print("Ошибка")
-        break
-    else:
-        fbs = fb1 + fb2
-        fb1 = fb2
-        fb2 = fbs
-        c+=1
-t = fb2%10
+t = a[0]
+if t < 0 or t > 10**7: print("Ошибка")
+t2 = LF(t)
 
 with open("n4_output_n3", "w") as z:
-    z.write(str(t))
+    z.write(str(LF(t2)))
     z.close()
 print(time.time()-t_start)
